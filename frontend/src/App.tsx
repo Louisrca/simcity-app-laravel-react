a8f0a76682688a2d23f77b7cfc87d97fe900f14a
Louis R <roccalouis26@gmail.com>
2023-08-01 15:42:26 +0200

    dependencies install: talwind installed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <div className="max-w-6xl mx-auto">
       <Routes>
         <Route
           path="/"
