a2979c4acd86b07ba5366c47cf727ec4b4ed96e3
Louis R. <roccalouis26@gmail.com>
2023-08-29 18:09:32 +0200

    feat: set up ChartCard Layout

---
+++ frontend/src/components/chartCart/ChartCard.tsx
@@ -0,0 +1,10 @@
+import { ChartCardLayout } from "../layout/chartCard/ChartCardLayout";
+export const ChartCard = () => {
+  return (
+    <>
+      <ChartCardLayout>
+        <div>ChartCard</div>
+      </ChartCardLayout>
+    </>
+  );
+};
--- frontend/src/components/pages/Dashboard/Dashboard.tsx
+++ frontend/src/components/pages/Dashboard/Dashboard.tsx
@@ -3,6 +3,7 @@ import { BtsTableFM } from "../../table/bts/BtsTableFM";
 import { BtsTableAGH } from "../../table/bts/BtsTableAGH";
 import s from "./Dashboard.module.css";
 import { useAuth } from "../../context/auth/AuthContext";
+import { ChartCard } from "../../chartCart/ChartCard";
 
 export const Dashboard = () => {
   const { meData, error } = useAuth();
@@ -11,6 +12,7 @@ export const Dashboard = () => {
   return (
     <div className={s.dashboardView}>
       <h1>Dashboard</h1>
+      <ChartCard />
       {user && (
         <h4>Connecté en tant que {user.firstname + " " + user.lastname}</h4>
       )}
