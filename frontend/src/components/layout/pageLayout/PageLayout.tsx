696859b39663e65a37f1b699341d4625c15b55a0
Louis R. <roccalouis26@gmail.com>
2023-08-29 11:06:25 +0200

    change folder name

--- frontend/src/components/layout/pageLayout/PageLayout.tsx
+++ frontend/src/components/layout/pageLayout/PageLayout.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren } from "react";
-import { Navbar } from "../../navbar/Navbar";
+import { Navbar } from "../../nav/Navbar";
 import s from "./PageLayout.module.css";
 
 export const PageLayout = ({ children }: PropsWithChildren) => {
