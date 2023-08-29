632ed83dece8be2df5ecfdb72559b04c7358235a
Louis R. <roccalouis26@gmail.com>
2023-08-29 11:42:17 +0200

    image vercel wip fixation with quote #5

--- frontend/src/components/auth/authLogin/AuthLogin.tsx
+++ frontend/src/components/auth/authLogin/AuthLogin.tsx
@@ -12,7 +12,7 @@ import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import LogoSimCity from "src/assets/LogoSimCity.jpg";
+import LogoSimCity from "/src/assets/LogoSimCity.jpg";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 function Copyright(props: any) {
